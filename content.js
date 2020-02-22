19cfe6c9ab931670c5abf03afd92a8fe3ad48205
LostAbaddon <lostabaddon@foxmail.com>
2020-02-22 14:28:02 +0800

    增加对AMSmath和AMSsymbols的支持

--- content.js
+++ content.js
@@ -3,11 +3,19 @@
 
 	var config = newEle('script');
 	config.type = 'text/x-mathjax-config';
-	config.innerHTML = 'MathJax.Hub.Config({ extensions: ["tex2jax.js"], jax: ["input/TeX","output/HTML-CSS"], tex2jax: {inlineMath: [["$","$"],["\\\\(","\\\\)"]]} });';
+	config.innerHTML = `MathJax.Hub.Config({
+		extensions: ["tex2jax.js"],
+		TeX: {
+			extensions: ["AMSmath.js", "AMSsymbols.js"]
+		},
+		jax: ["input/TeX", "output/HTML-CSS"],
+		tex2jax: {
+			inlineMath: [["$","$"]]}
+		});`;
 	document.body.appendChild(config);
 
 	var mathjax = newEle('script');
 	mathjax.type = 'text/javascript';
-	mathjax.src = chrome.extension.getURL('MathJax2.5/MathJax.js');
+	mathjax.src = chrome.extension.getURL('MathJax2.5/MathJax.js?config=TeX-AMS_HTML');
 	document.body.appendChild(mathjax);
 }) (window);
